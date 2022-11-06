a9784ae8cd06e4ade7a5fbf5522178817dc8cb97
BananaMasta <53840462+BananaMasta@users.noreply.github.com>
2022-11-06 12:51:15 +0400

    Homework5 Re-work

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1() {
@@ -19,14 +20,13 @@ public class Main {
     }
 
     public static void task2() {
-        int clientDeviceYear = 2015;
+        int clientDeviceYear = 2016;
         int clientOS = 1;
         if (clientOS == 0 && clientDeviceYear == 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOS == 0) {
             System.out.println("Установите версию для iOS по ссылке");
-        }
-        if (clientOS == 1 && clientDeviceYear == 2015) {
+        } else if (clientOS == 1 && clientDeviceYear == 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1) {
             System.out.println("Установите версию для Android по ссылке");
@@ -54,33 +54,49 @@ public class Main {
             System.out.println("Доставки нету");
         }
     }
-    public static void task5(){
+
+    public static void task5() {
         int monthYear = 12;
-        switch (monthYear){
+        switch (monthYear) {
+            default:
+                System.out.println("Такого месяца нету");
+                break;
             case 1:
                 System.out.println("Январь");
+                break;
             case 2:
                 System.out.println("Февраль");
+                break;
             case 3:
                 System.out.println("Март");
+                break;
             case 4:
                 System.out.println("Апрель");
+                break;
             case 5:
                 System.out.println("Май");
+                break;
             case 6:
                 System.out.println("Июнь");
+                break;
             case 7:
                 System.out.println("Июль");
+                break;
             case 8:
                 System.out.println("Август");
+                break;
             case 9:
                 System.out.println("Сентябрь");
+                break;
             case 10:
                 System.out.println("Октябрь");
+                break;
             case 11:
                 System.out.println("Ноябрь");
+                break;
             case 12:
                 System.out.println("Декабрь");
+                break;
         }
     }
 }
